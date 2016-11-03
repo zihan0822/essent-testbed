b44085cb68f2b9f23367e59ad7befc337f821e52
Scott Beamer <sbeamer@lbl.gov>
2016-11-03 15:11:58 -0700

    add wide signal tests for dynamic shifts and add

--- src/main/scala/BigUInt.scala
+++ src/main/scala/BigUInt.scala
@@ -14,7 +14,7 @@ class BigUInt(val w: Int) extends Module {
     val inB = Input(UInt(width = w))
     val inShamt = Input(UInt(width = 4))
     val outPassA = Output(UInt(width = w))
-    val outAPlusB = Output(UInt(width = w+1))
+    val outAPlusB = Output(UInt(width = w))
     // val outASubB = Output(SInt(width = w+1))
     val outAMulB = Output(UInt(width = w+w))
     val outADivB = Output(UInt(width = w))
@@ -27,8 +27,8 @@ class BigUInt(val w: Int) extends Module {
     val outANeqB = Output(UInt(width = 1))
     val outAShl = Output(UInt(width = w+shamt))
     val outAShr = Output(UInt(width = w-shamt))
-    // val outADshl = Output(UInt(width = w+shamt))
-    // val outADshr = Output(UInt(width = w-shamt))
+    val outADshl = Output(UInt(width = w+shamt))
+    val outADshr = Output(UInt(width = w-shamt))
     // val outNegA = Output(SInt(width = w+1))
     // val outNotA = Output(UInt(width = w))
     val outAAndB = Output(UInt(width = w))
@@ -50,8 +50,8 @@ class BigUInt(val w: Int) extends Module {
   io.outANeqB := io.inA =/= io.inB
   io.outAShl := io.inA << shamt
   io.outAShr := io.inA >> shamt
-  // io.outADshl := io.inA << io.inShamt
-  // io.outADshr := io.inA >> io.inShamt
+  io.outADshl := io.inA << io.inShamt
+  io.outADshr := io.inA >> io.inShamt
   // io.outNegA := -io.inA
   // io.outNotA := ~io.inA
   io.outAAndB := io.inA & io.inB
@@ -64,11 +64,12 @@ class BigUIntTests(c: BigUInt) extends PeekPokeTester(c) {
   for (i <- 0 until 4) {
     val a = BigInt(c.w, Random)
     val b = BigInt(c.w, Random)
+    val mask = (BigInt(1) << c.w) - 1
     poke(c.io.inA, a)
     poke(c.io.inB, b)
     poke(c.io.inShamt, c.shamt)
     expect(c.io.outPassA, a)
-    expect(c.io.outAPlusB, a + b)
+    expect(c.io.outAPlusB, (a + b) & mask)
     // expect(c.io.outASubB, a - b)
     expect(c.io.outAMulB, a * b)
     expect(c.io.outADivB, a / b)
@@ -81,8 +82,8 @@ class BigUIntTests(c: BigUInt) extends PeekPokeTester(c) {
     expect(c.io.outANeqB, a != b)
     expect(c.io.outAShl, a << c.shamt)
     expect(c.io.outAShr, a >> c.shamt)
-    // expect(c.io.outADshl, a << c.shamt)
-    // expect(c.io.outADshr, a >> c.shamt)
+    expect(c.io.outADshl, a << c.shamt)
+    expect(c.io.outADshr, a >> c.shamt)
     // expect(c.io.outNegA, -a)
     // expect(c.io.outNotA, ~a)
     expect(c.io.outAAndB, a & b)
