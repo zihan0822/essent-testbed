c4ad83d92edc7ef6a26ef07a392fe6920e91c408
Scott Beamer <sbeamer@lbl.gov>
2017-02-22 22:42:45 -0800

    debug api support for sint

--- csrc/comm_wrapper.h
+++ csrc/comm_wrapper.h
@@ -82,6 +82,25 @@ private:
   UInt<w> *ui_ptr_;
 };
 
+template<int w>
+class sint_wrapper_t : public sig_wrapper_t {
+public:
+  sint_wrapper_t() {}
+  sint_wrapper_t(SInt<w> *si_ptr) : si_ptr_(si_ptr) {}
+
+  virtual size_t get_num_words() { return (w+63) / 64; }
+  virtual size_t get_value(uint64_t* values) {
+    si_ptr_->raw_copy_out(values);
+    return get_num_words();
+  }
+  virtual size_t put_value(uint64_t* values) {
+    si_ptr_->raw_copy_in(values);
+    return get_num_words();
+  }
+private:
+  SInt<w> *si_ptr_;
+};
+
 template<typename DUT_t_>
 class CommWrapper: public sim_api_t<sig_wrapper_t*> {
 public:
@@ -119,6 +138,11 @@ public:
     sim_data.inputs.push_back(new uint_wrapper_t<w>(sig_ptr));
   }
 
+  template<int w>
+  void add_in_signal(SInt<w> *sig_ptr) {
+    sim_data.inputs.push_back(new sint_wrapper_t<w>(sig_ptr));
+  }
+
   void add_out_signal(uint64_t *sig_ptr) {
     sim_data.outputs.push_back(new sig_wrapper_t(sig_ptr));
   }
@@ -132,6 +156,11 @@ public:
     sim_data.outputs.push_back(new uint_wrapper_t<w>(sig_ptr));
   }
 
+  template<int w>
+  void add_out_signal(SInt<w> *sig_ptr) {
+    sim_data.outputs.push_back(new sint_wrapper_t<w>(sig_ptr));
+  }
+
   void add_signal(uint64_t *sig_ptr) {
     sim_data.signals.push_back(new sig_wrapper_t(sig_ptr));
   }
@@ -143,6 +172,11 @@ public:
   template<int w>
   void add_signal(UInt<w> *sig_ptr) {
     sim_data.signals.push_back(new uint_wrapper_t<w>(sig_ptr));
+  }
+
+  template<int w>
+  void add_signal(SInt<w> *sig_ptr) {
+    sim_data.signals.push_back(new sint_wrapper_t<w>(sig_ptr));
   }
 
   void map_signal(std::string label, size_t index) {
