6a656ded3588f059169ddef524de4b412a185d1e
Scott Beamer <sbeamer@ucsc.edu>
2023-01-30 18:57:51 -0800

    modernize top.cc with newest from riscv-mini

--- riscv-mini/top.cc
+++ riscv-mini/top.cc
@@ -1,12 +1,11 @@
+#include <cstdint>
 #include <iostream>
-#include <fcntl.h>
-#include <signal.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <uint.h> 
-#include "Tile.h"
+
 #include "mm.h"
+#include "uint.h" 
+#include "Tile.h"
+ 
+// Adapted from riscv-mini/src/main/cc/top.cc
  
 using namespace std;
 
@@ -15,9 +14,10 @@ static uint64_t main_time = 0;
 Tile *top;
 mm_magic_t* mem;
 
+
 void tick(bool verbose, bool done_reset) {
   main_time++;
-  
+
   top->io_nasti_aw_ready = UInt<1>(mem->aw_ready());
   top->io_nasti_ar_ready = UInt<1>(mem->ar_ready());
   top->io_nasti_w_ready = UInt<1>(mem->w_ready());
@@ -51,35 +51,56 @@ void tick(bool verbose, bool done_reset) {
     top->io_nasti_b_ready 
   );
   main_time++;
-
 }
 
+
 int main(int argc, char** argv) {
-  uint64_t timeout = 1000L;
+  uint64_t timeout = 10000000L;
   top = new Tile;
   mem = new mm_magic_t(1L << 32, 8);
   cout << "Enabling waves..." << endl;
-  load_mem(mem->get_data(), (const char*)(argv[1])); 
+  load_mem(mem->get_data(), (const char*)(argv[1]));
+
+  // reset
   top->reset = UInt<1>(1);
   cout << "Starting simulation!" << endl;
   for (size_t i = 0; i < 5 ; i++) {
     tick(true, false); 
   }
-
   top->reset = UInt<1>(0);
+
+  // actual sim
   top->io_host_fromhost_bits = UInt<32>(0);
   top->io_host_fromhost_valid = UInt<1>(0);
-  cout << "while" <<endl;
   do {
     tick(true,true);
   } while(!top->io_host_tohost.as_single_word() && main_time < timeout);
 
-  for (size_t i = 0 ; i < 10 ; i++) {
+  int retcode = top->io_host_tohost.as_single_word() >> 1;
+
+  // run 10 cycles past termination
+  // FUTURE: small off-by-1 hack in detecting termination
+  for (size_t i = 0 ; i < 9 ; i++) {
     tick(true, true); 
+  }
+
+  // note: don't know why riscv-mini /10 (instead of /2), but same for testing
+  if (main_time >= timeout) {
+    cerr << "Simulation terminated by timeout at time " << main_time
+         << " (cycle " << main_time / 10 << ")"<< endl;
+    return -1;
+  } else {
+    cerr << "Simulation completed at time " << main_time
+         << " (cycle " << main_time / 10 << ")"<< endl;
+    if (retcode) {
+      cerr << "TOHOST = " << retcode << endl;
+    }
   }
 
   delete top;
   delete mem;
-  return 0;
 
+  cout << "Finishing simulation!\n";
+
+  return 0;
 }
